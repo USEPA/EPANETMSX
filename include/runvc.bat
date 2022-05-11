fad3630c8931db9a2c8856e614df55de02372e37
fengshang1972 <shang.feng@epa.gov>
2022-05-11 11:37:13 -0400

    Update runvc.bat

--- include/runvc.bat
+++ include/runvc.bat
@@ -4,4 +4,4 @@ cd "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\"
 for /F "tokens=* USEBACKQ" %%F in (`vswhere.exe -latest -property installationPath`) do (set VSDIR=%%F)
 call "%VSDIR%\Common7\Tools\VsDevCmd.bat" -arch=%2% > nul
 cd %curdir%
-CL /O2 /LD /nologo %1% 
+CL /O2 /LD /nologo %1%
